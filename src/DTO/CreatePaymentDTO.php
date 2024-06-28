661d76505c1cfc6626466fd20bb2ef5557d8a8f1
mateusdeitos <mate.deitos@hotmail.com>
2024-06-27 23:26:46 -0300

    documenting

--- src/DTO/CreatePaymentDTO.php
+++ src/DTO/CreatePaymentDTO.php
@@ -8,24 +8,42 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class CreatePaymentDTO {
 
+	/**
+	 * The amount in cents
+	 */
 	#[Assert\NotBlank()]
 	#[Assert\Positive()]
 	public int $amount = 0;
 
+	/**
+	 * The currency code, e.g. EUR
+	 */
 	#[Assert\NotBlank()]
 	#[Assert\Length(exactly: 3)]
 	public string $currency = "";
 
+	/**
+	 * The card number without spaces
+	 */
 	#[Assert\NotBlank()]
 	public string $cardNumber = "";
 
+	/**
+	 * The card expiration year as 4 digits
+	 */
 	#[Assert\NotBlank()]
 	public int $cardExpYear = 0;
 
+	/**
+	 * The card expiration month as 2 digits
+	 */
 	#[Assert\NotBlank()]
 	#[Assert\Range(min: 1, max: 12)]
 	public int $cardExpMonth = 0;
 
+	/**
+	 * The card CVV
+	 */
 	#[Assert\NotBlank()]
 	public int $cardCvv = 0;
 
